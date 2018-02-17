2281565c2c7aab4254561d4059eb93fbe4e7631c
Unknown <omarfoe94@gmail.com>
2018-02-17 14:28:38 +0200

    Added smooth animation functions

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { Animated } from 'react-native';
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { Animated } from "react-native";
 
 export default class Label extends PureComponent {
   static defaultProps = {
@@ -9,7 +9,7 @@ export default class Label extends PureComponent {
     active: false,
     focused: false,
     errored: false,
-    restricted: false,
+    restricted: false
   };
 
   static propTypes = {
@@ -33,50 +33,88 @@ export default class Label extends PureComponent {
 
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
+      PropTypes.node
+    ])
   };
 
   constructor(props) {
     super(props);
 
+    this.containerWidth = 0;
+
     this.state = {
-      input: new Animated.Value(this.inputState()),
-      focus: new Animated.Value(this.focusState()),
+      inputTX: new Animated.Value(this.inputTXState()),
+      inputTY: new Animated.Value(this.inputTYState()),
+      inputScale: new Animated.Value(this.inputScaleState()),
+      focus: new Animated.Value(this.focusState())
     };
   }
 
   componentWillReceiveProps(props) {
-    let { focus, input } = this.state;
+    let { focus, inputTY, inputScale, inputTX } = this.state;
     let { active, focused, errored, animationDuration: duration } = this.props;
 
     if (focused ^ props.focused || active ^ props.active) {
-      let toValue = this.inputState(props);
-
-      Animated
-        .timing(input, { toValue, duration })
-        .start();
+      Animated.parallel([
+        Animated.timing(inputTX, {
+          toValue: this.inputTXState(props),
+          duration,
+          useNativeDriver: true
+        }),
+        Animated.timing(inputTY, {
+          toValue: this.inputTYState(props),
+          duration,
+          useNativeDriver: true
+        }),
+        Animated.timing(inputScale, {
+          toValue: this.inputScaleState(props),
+          duration,
+          useNativeDriver: true
+        })
+      ]).start();
     }
 
     if (focused ^ props.focused || errored ^ props.errored) {
       let toValue = this.focusState(props);
 
-      Animated
-        .timing(focus, { toValue, duration })
-        .start();
+      Animated.timing(focus, { toValue, duration }).start();
     }
   }
 
-  inputState({ focused, active } = this.props) {
-    return active || focused? 1 : 0;
+  inputTYState(
+    { focused, active, baseSize, fontSize, basePadding, activeFontSize } = this
+      .props
+  ) {
+    return active || focused
+      ? baseSize - basePadding - activeFontSize * 1.2
+      : baseSize + fontSize * 0.25;
+  }
+
+  inputScaleState({ focused, active, fontSize, activeFontSize } = this.props) {
+    return active || focused ? activeFontSize / fontSize : 1;
+  }
+
+  inputTXState({ focused, active } = this.props) {
+    return active || focused ? -this.getTranslationX() : 0;
+  }
+
+  getTranslationX({ fontSize, activeFontSize } = this.props) {
+    return this.containerWidth * (1 - activeFontSize / fontSize) / 2;
   }
 
   focusState({ focused, errored } = this.props) {
-    return errored? -1 : (focused? 1 : 0);
+    return errored ? -1 : focused ? 1 : 0;
+  }
+
+  onContainerLayout(event) {
+    this.containerWidth = event.nativeEvent.layout.width;
+    if (!this.props.focused && this.props.active) {
+      this.setState({ inputTX: new Animated.Value(-this.getTranslationX()) });
+    }
   }
 
   render() {
-    let { focus, input } = this.state;
+    let { focus } = this.state;
     let {
       children,
       restricted,
@@ -90,7 +128,7 @@ export default class Label extends PureComponent {
       basePadding,
       style,
       errored,
-      active, 
+      active,
       focused,
       animationDuration,
       ...props
@@ -107,30 +145,28 @@ export default class Label extends PureComponent {
           ]
         });
 
-    let top = input.interpolate({
-      inputRange: [0, 1],
-      outputRange: [
-        baseSize + fontSize * 0.25,
-        baseSize - basePadding - activeFontSize,
-      ],
-    });
-
     let textStyle = {
-      fontSize: input.interpolate({
-        inputRange: [0, 1],
-        outputRange: [fontSize, activeFontSize],
-      }),
-
-      color,
+      fontSize,
+      color
     };
 
+    let transform = [
+      { translateX: this.state.inputTX },
+      { translateY: this.state.inputTY },
+      { scale: this.state.inputScale }
+    ];
+
     let containerStyle = {
-      position: 'absolute',
-      top,
+      position: "absolute",
+      top: 0,
+      transform
     };
 
     return (
-      <Animated.View style={containerStyle}>
+      <Animated.View
+        style={containerStyle}
+        onLayout={this.onContainerLayout.bind(this)}
+      >
         <Animated.Text style={[style, textStyle]} {...props}>
           {children}
         </Animated.Text>
